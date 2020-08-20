d620c9370c7a630e9c6b8f527132f2e5447657de
Venkatesh Nandikolla <45224319+venkatesh2506@users.noreply.github.com>
2020-08-20 08:28:46 +0530

    Update settings.py

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'q2lra#ddiscy17p3t-&^m1+ic#xba!6zkyfe-&*d#+vh!%q*ev'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [venkatesh2506.pythonanywhere.com,]
 
 
 # Application definition
